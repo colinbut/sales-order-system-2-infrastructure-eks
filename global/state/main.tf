34dbd74de230978e594a71573a8642e537663b2e
Colin But <colin.but@outlook.com>
2020-04-21 20:05:32 +0100

    create s3 bucket to store the state and dynamodb table for the state lock

---
+++ global/state/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_dynamodb_table" "s3-state-lock-table" {
+    name = "sales-order-system-eks-s3-state-lock"
+
+    hash_key        = "LockID"
+    billing_mode    = "PAY_PER_REQUEST"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
+resource "aws_s3_bucket" "terraform_state_s3_bucket" {
+    bucket = "sales-order-system-eks-state-s3-bucket"
+
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    versioning {
+        enabled = true
+    }
+
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
